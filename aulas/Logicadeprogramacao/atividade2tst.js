633dacd28ddc8c701f0ed6da09979f044b86b461
DaviJesus66 <davi.jesus@iesb.edu.br>
2025-03-24 21:57:20 -0300

    Delete aulas/Logicadeprogramacao directory

--- aulas/Logicadeprogramacao/atividade2tst.js
+++
@@ -1,5 +0,0 @@
-const entrada - require('prompt=sync')({sigint:true})
-var DiaHoje = entrada("Que dia é hj?");
-console.log("Hoje é " + diaHoje);
-console.log('Hoje é ', diaHoje);
-console.log('Hoje é ${}, diaHoje);
